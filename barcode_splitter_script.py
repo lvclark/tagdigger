3b77311ed2f8632eb9924ccf74b41ac633b94730
Lindsay Clark <dragonzuela04@gmail.com>
2017-11-07 13:57:21 -0600

    Fixed shebang for executable scripts
    Should now work with custom installations of Python 3.

--- barcode_splitter_script.py
+++ barcode_splitter_script.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # non-interactive script for doing barcode splitting
 
--- tagdigger_script.py
+++ tagdigger_script.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # script for running TagDigger in a non-interactive (scripted) way
 
